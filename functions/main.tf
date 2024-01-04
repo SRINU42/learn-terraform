e459af56cb6b154c3d445fa918925ba0b460e960
SRINU <vasu_anand01@yahoo.co.in>
2024-01-04 21:37:09 +0530

    functions

--- functions/main.tf
+++ functions/main.tf
@@ -6,10 +6,10 @@ output "class" {
     value = var.class
 }
 
-output "class" {
+output "class1" {
     value = upper[ var.class ]
 }
 
-output "class" {
+output "class2" {
     value = lower[ var.class ]
 }
